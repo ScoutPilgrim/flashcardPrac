3fe44519e8e7567292d152ff74796f5d0af14ea8
Ian Christner and Svitlana Filatova <ian+svitlana@>
2019-03-26 14:24:38 -0700

    updated JS logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
-let showAnswer = false;
 $(document).ready(function() {
   $(".clickable").click(function() {
-      console.log($(this));
-      if(!showAnswer){
-        $(this).addClass('showHidden');
-        showAnswer = toggleFlag(showAnswer);
-      }else{
-        $(this).removeClass('showHidden');
-        showAnswer = toggleFlag(showAnswer);
-      }
-    });
-    function toggleFlag(myFlag){
-      if(myFlag === true){
-        myFlag = false;
-      }else{
-        myFlag = true;
-      }
-      return myFlag;
-    }
+    console.log($(this));
+    /*$(this).toggleClass('showHidden')*/
+    $(this).find('.fuckBootstrap').toggle();
   });
+});
